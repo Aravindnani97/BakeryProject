caa80d42077c33df2db78b33fc892b9f7abb9bb3
Aravindnani97 <aravind1123045@gmail.com>
2020-09-28 11:10:51 +0530

    ProductsController is Added

---
+++ BakeryProject/Controllers/ProductsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BakeryProject.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult NewAction()
+        {
+            return View();
+        }
+    }
+}
